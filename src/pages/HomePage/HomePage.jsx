b4740dad2427984b29fdc487ae95575762656359
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2023-03-21 13:58:23 +0600

    bind to mockApi

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Basket from '../../components/Basket/Basket'
@@ -10,51 +10,22 @@ import classes from './homePage.module.css'
 
 function HomePage()
 {   
-    const data = [
+    const [data, setData] = useState([])
+    useEffect(() =>
+    {
+        fetch(`https://63d78ffe5c4274b136f6a651.mockapi.io/items`)
+        .then((res) =>
         {
-            id: 1,
-            price: 120,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis eaque fuga magnam iure. Tempore eveniet quis itaque recusandae, aliquam totam soluta neque labore. Alias eaque optio quas aliquam maiores?',
-            title: 'Рыба',
-            img: 'https://eda.ru/img/eda/c270x270/s1.eda.ru/StaticContent/Photos/140201143522/140208000700/p_O.jpg.webp'
-        },
-        {
-            id: 2,
-            price: 150,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aliquid earum iusto suscipit quam impedit labore at debitis reiciendis rem hic, cupiditate quis itaque vero tenetur officia. Possimus, quia quod!',
-            title: 'Котлетки',
-            img: 'https://eda.ru/img/eda/c270x270/s1.eda.ru/StaticContent/Photos/120213175433/140114142653/p_O.jpg.webp'
-        },
-        {
-            id: 3,
-            price: 200,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aliquid earum iusto suscipit quam impedit labore at debitis reiciendis rem hic, cupiditate quis itaque vero tenetur officia. Possimus, quia quod!',
-            title: 'Плов',
-            img: 'https://eda.ru/img/eda/c270x270/s1.eda.ru/StaticContent/Photos/120131085017/171024233545/p_O.jpg.webp'
-        },
-        {
-            id: 4,
-            price: 250,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aliquid earum iusto suscipit quam impedit labore at debitis reiciendis rem hic, cupiditate quis itaque vero tenetur officia. Possimus, quia quod!',
-            title: 'Чечевица',
-            img: 'https://eda.ru/img/eda/c270x270/s1.eda.ru/StaticContent/Photos/150810204910/150815154033/p_O.jpg.webp'
-        },
-        {
-            id: 5,
-            price: 220,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aliquid earum iusto suscipit quam impedit labore at debitis reiciendis rem hic, cupiditate quis itaque vero tenetur officia. Possimus, quia quod!',
-            title: 'Куринное филе',
-            img: 'https://eda.ru/img/eda/c270x270/s1.eda.ru/StaticContent/Photos/120213185949/120214124324/p_O.jpg.webp'
-        },
-        {
-            id: 6,
-            price: 180,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aliquid earum iusto suscipit quam impedit labore at debitis reiciendis rem hic, cupiditate quis itaque vero tenetur officia. Possimus, quia quod!',
-            title: 'Баклажаны с Моцарелой',
-            img: 'https://eda.ru/img/eda/c270x270/s1.eda.ru/StaticContent/Photos/120213175134/1202131752230/p_O.jpg.webp'
-        },
-    ]
-    const categories = ['Завтраки','Блюда','Десерты','Напитки']
+            return res.json();
+            }).then((json) =>
+            {
+                console.log(json[1]);
+                setData(json)
+            })
+    }, [])
+    
+    
+    const categories = ['Завтраки','Блюда','Напитки', 'Десерты']
     const dispatch = useDispatch();
     let count = 0;
     let totalSumma = 0;
@@ -85,15 +56,6 @@ function HomePage()
                 calc ? <Calculator totalSumma={totalSumma} calc={calc} setCalc={setCalc} />
                     :
                 <div className={classes.home}>
-                    {/* <div>  
-                        <button onClick={()=> dispatch(addToCart(data[0]))}>ad1</button>
-                        <button onClick={()=> dispatch(addToCart(data[1]))}>ad2</button>
-                        <button onClick={()=> dispatch(removeCart(1))}>удалить пролудкт</button>
-                        <button onClick={()=> dispatch(increaseCart(1))}>+</button>
-                        <button onClick={()=> dispatch(decrementCart(1))}>-</button>
-                        <button onClick={()=> dispatch(clearCart())}>delete</button>
-                        <button onClick={()=> console.log(data1)}>show result</button>
-                    </div> */}
                     <h2 className={classes.title}>Меню</h2>
                     <div className={classes.filter}>      
                         <ul>
@@ -102,7 +64,7 @@ function HomePage()
                     </div>
                     <div className={classes.foods}>
                         {
-                            data.map(el => <Food key={el.id} product={el} />)
+                            data[category]?.map(el => <Food key={el.id} product={el} />)
                         }
                     
                     </div>
