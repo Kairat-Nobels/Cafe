dc3d3e8beaab9e60ce97eabd4c966893361019de
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2023-04-10 14:59:35 +0600

    finish all project, i ges

--- src/redux/actions/about.jsx
+++ src/redux/actions/about.jsx
@@ -5,8 +5,7 @@ export const getAbout = () =>
     return async(dispatch) =>
     {
 
-        // const response = await fetch(`https://6421dd5286992901b2bdedd9.mockapi.io/items`)
-        const response = await fetch(`https://63d78ffe5c4274b136f6a651.mockapi.io/items`)
+        const response = await fetch(`https://6421dd5286992901b2bdedd9.mockapi.io/items`)
         const resData = await response.json();
         dispatch({
             type: GET_ABOUT,
